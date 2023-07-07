e57495f4807d0b8a71285d7aab422333af1a7359
Markus Volk <f_l_k@t-online.de>
2023-07-07 16:11:40 +0200

    flathub.sh: install QGnomePlatform

--- recipes-extended/system-config/files/flathub.sh
+++ recipes-extended/system-config/files/flathub.sh
@@ -5,3 +5,4 @@ flatpak remote-add --if-not-exists --user flathub-beta https://flathub.org/beta-
 flatpak remote-add --if-not-exists --user gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
 flatpak install --assumeyes --user org.gtk.Gtk3theme.adw-gtk3
 flatpak install --assumeyes --user org.gtk.Gtk3theme.adw-gtk3-dark
+flatpak install --assumeyes --user flathub org.kde.PlatformTheme.QGnomePlatform/x86_64/6.5
